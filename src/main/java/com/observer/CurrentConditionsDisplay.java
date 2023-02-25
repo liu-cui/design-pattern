b3d2f8194f9c0e9cffae6d14355553988a8ab031
liucui <15301638120@163.com>
2023-02-25 14:46:13 +0800

    observer pattern

---
+++ src/main/java/com/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package com.observer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CurrentConditionsDisplay implements Observer, DisplayElement{
+    private double temperature;
+    private double humidity;
+    private double pressure;
+    public Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        log.info("Current conditions: " + temperature + " F degrees and " + humidity + " % humidity and " + pressure + " pressure");
+    }
+
+    @Override
+    public void update(double temperature, double humidity, double pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+}
---
+++ src/main/java/com/observer/WeatherStationSimulator.java
@@ -0,0 +1,12 @@
+package com.observer;
+
+public class WeatherStationSimulator {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay conditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(19, 97, 0);
+        weatherData.setMeasurements(19, 97, 7);
+        weatherData.setMeasurements(19, 97, 3);
+        weatherData.setMeasurements(19, 97, 1);
+    }
+}
